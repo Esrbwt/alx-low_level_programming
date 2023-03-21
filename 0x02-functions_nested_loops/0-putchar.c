b5b156a20aa2d6e78f2ca96c2fdc04e16218f6c1
Esrom Woldtensaie <esrbwt19@gmail.com>
2023-03-21 07:06:53 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,12 @@
 
 /**
  * main - Entry point
- *
- * Description: 'prints _putchar to stdout'
- *
- * Return: always 0
+ * Return: Always 0
  */
 
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
