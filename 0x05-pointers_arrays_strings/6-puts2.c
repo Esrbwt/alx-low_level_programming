3a0632f6ec69d9ec3f8fccff6e0034105a2a9f31
Esrom Woldtensaie <esrbwt19@gmail.com>
2023-03-27 14:45:33 -0700

    Half the lies they tell about me arent true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,8 +3,8 @@
 /**
  * puts2 - prints every other character of a string, starting with the first
  * character, followed by a new line.
- *
  * @str: input string
+ * Return: 0
  */
 void puts2(char *str)
 {
