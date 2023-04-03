2c07e8590fef4405acbb8f5366658ac0f2cc20be
Esrom Woldtensaie <esrbwt19@gmail.com>
2023-04-03 11:07:11 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+void simple_print_buffer(char *buffer, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
+
